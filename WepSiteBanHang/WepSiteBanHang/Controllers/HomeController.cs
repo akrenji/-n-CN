96b74f00f5d12c3201c98bd13ee917018f908708
Nhanpc <Nhanpc@NHAN>
2016-10-25 22:05:44 +0700

    Bản Chính

---
+++ WepSiteBanHang/WepSiteBanHang/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WepSiteBanHang.Models;
+namespace WepSiteBanHang.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbWepSiteBanHangDataContext db = new dbWepSiteBanHangDataContext();
+        public ActionResult Index()
+        {
+            //List LapTop mới nhất
+            var lstLT = db.SanPhams.Where(n => n.MaLoaiSP == 3 && n.Moi == 1);
+            //Gán vào ViewBag
+            ViewBag.ListLTM = lstLT;
+            var lstDTM = db.SanPhams.Where(n => n.MaLoaiSP == 1 && n.Moi == 1 && n.DaXoa == false);
+            //Gán vào ViewBag
+            ViewBag.ListDTM = lstDTM;
+            var lstMTB = db.SanPhams.Where(n => n.MaLoaiSP == 2 && n.Moi == 1 && n.DaXoa == false);
+            //Gán vào ViewBag
+            ViewBag.ListMTBM = lstMTB;
+            return View();
+        }
+        public ActionResult MenuPartial()
+        {
+            //Truy vấn lấy về 1 list các sản phẩm
+            var lstSP = db.SanPhams;
+            return PartialView(lstSP);
+        }
+
+
+
+    }
+}
---
+++ WepSiteBanHang/WepSiteBanHang/Controllers/SanPhamController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WepSiteBanHang.Models;
+namespace WepSiteBanHang.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        dbWepSiteBanHangDataContext db = new dbWepSiteBanHangDataContext();
+        // GET: SanPham
+        public ActionResult SanPhamStyle1Partial()
+        {
+            return PartialView();
+        }
+        public ActionResult XemChiTiet(int ?id)
+        {
+            if(id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == id && n.DaXoa==false);
+            if(sp==null)
+            {
+                return HttpNotFound();
+            }
+            return View(sp);
+        }
+        public ActionResult SanPham(int ? MaLoaiSP, int? MaNSX)
+        {
+            if(MaLoaiSP == null || MaNSX== null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+           
+            var lstSP = db.SanPhams.Where(n => n.MaLoaiSP == MaLoaiSP && n.MaNSX == MaNSX);
+            if (lstSP.Count()==0)
+            {
+                return HttpNotFound();
+            }
+                return View(lstSP);
+        }
+    }
+
+
+}
