064d2763be3477201bd8c8b978592c0b7838640f
Nhanpc <Nhanpc@NHAN>
2016-11-05 01:54:44 +0700

    da sua xong div tim kiem

--- WepSiteBanHang/WepSiteBanHang/Models/dbQuanLyEntity.Designer.cs
+++ WepSiteBanHang/WepSiteBanHang/Models/dbQuanLyEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\admin\Desktop\-n-CN-master\-n-CN-master\WepSiteBanHang\WepSiteBanHang\Models\dbQuanLyEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'F:\DACN\DACN\-n-CN\WepSiteBanHang\WepSiteBanHang\Models\dbQuanLyEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
